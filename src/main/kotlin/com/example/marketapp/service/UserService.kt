7858354ae1c028e25c80724c4f3bed2cf10992be
wylee <wylee@wemakeprice.com>
2023-05-16 17:03:39 +0900

    rest api 설정

---
+++ src/main/kotlin/com/example/marketapp/service/UserService.kt
@@ -0,0 +1,17 @@
+package com.example.marketapp.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class UserService {
+
+    fun login() {
+
+    }
+
+    fun logout() {
+
+    }
+
+
+}
